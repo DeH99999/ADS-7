2baaffbdc5925f9d629e47548087c1a3df10bc20
DeH99999 <vakasin-denis@mail.ru>
2026-05-30 01:50:10 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,41 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
-  while (count--)
-    train.addCar(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+  const int c = 1221;
+  std::srand(static_cast<unsigned>(std::time(nullptr)));
+  std::ofstream fOff("all_off.csv");
+  std::ofstream fOn("all_on.csv");
+  std::ofstream fRand("random.csv");
+  fOff << "n,ops\n";
+  fOn << "n,ops\n";
+  fRand << "n,ops\n";
+  for (int n = 1; n <= c; ++n) {
+    {
+      Train train;
+      for (int i = 0; i < n; ++i) train.addCar(false);
+      train.getLength();
+      fOff << n << "," << train.getOpCount() << "\n";
+    }
+    {
+      Train train;
+      for (int i = 0; i < n; ++i) train.addCar(true);
+      train.getLength();
+      fOn << n << "," << train.getOpCount() << "\n";
+    }
+    {
+      Train train;
+      for (int i = 0; i < n; ++i) train.addCar(std::rand() % 2);
+      train.getLength();
+      fRand << n << "," << train.getOpCount() << "\n";
+    }
+  }
+  fOff.close();
+  fOn.close();
+  fRand.close();
   return 0;
 }
